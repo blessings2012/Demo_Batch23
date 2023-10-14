90ed55e6b8f935f48aed4cee26c1299cf6d5aaa9
rekha ram <rekharam392@gmail.com>
2023-10-14 16:48:44 +0530

    comment removed

--- src/ABC/MethodOverriding.java
+++ src/ABC/MethodOverriding.java
@@ -27,12 +27,6 @@ class Parent{
 	//Static methods can not be overridden(method hiding)
 	//Final methods can not be overridden
 	//The overriding method must have same return type
-	/*
-	 * The access modifier for an overriding method can allow more, but not
-	 * less, access than the overridden method. For example, a protected instance
-	 * method in the super-class can be made public, but not private, in the
-	 * subclass.  
-	 * see program MethodOverridingAccessModifier
-	 */	
+	
 
 
